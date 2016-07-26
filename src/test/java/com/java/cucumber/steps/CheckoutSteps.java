25e6bd36512e4a1d7968be666d2b9dc70b1b743d
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-07-25 21:24:48 -0500

    First commit

---
+++ src/test/java/com/java/cucumber/steps/CheckoutSteps.java
@@ -0,0 +1,40 @@
+package com.java.cucumber.steps;
+
+import com.java.cucumber.impl.Checkout;
+import cucumber.api.CucumberOptions;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.junit.Cucumber;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by mgupta on 7/25/16.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        format = {"pretty", "html:target/cucumber"},
+        features = "classpath:features/checkout.feature"
+)
+public class CheckoutSteps {
+
+    Checkout checkout = new Checkout();
+
+    @Given("^the price of a \"([^\"]*)\" is (\\d+)c$")
+    public void the_price_of_a_is_c(String itemName, int price) throws Throwable {
+        checkout.addItemToStore(itemName, price);
+    }
+
+    @When("^I checkout (\\d+) \"([^\"]*)\"$")
+    public void i_checkout(int itemCount, String itemName) throws Throwable {
+        checkout.addItemToCheckout(itemCount, itemName);
+    }
+
+    @Then("^the total price should be (\\d+)c$")
+    public void the_total_price_should_be_c(int priceTotal) throws Throwable {
+        Assert.assertEquals(priceTotal, checkout.total());
+    }
+
+
+}
